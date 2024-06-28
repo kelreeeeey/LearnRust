122d07106f78a1f9b6c7f421688407a516eb7374
Taufiq Kelrey <88223451+kelreeeeey@users.noreply.github.com>
2024-06-28 07:29:39 +0700

    hello cargo 😁

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world!, changed");
+    println!("cargo new");
+    println!("cargo build");
+    println!("cargo run");
+    println!("cargo check");
 }
