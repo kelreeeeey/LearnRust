1d462709e94610793fa721ed0ba880171f02111d
Taufiq Kelrey <kelreytaufiq08@mail.ugm.ac.id>
2024-08-19 19:17:52 +0700

    learning struct, add associative function

--- learn_struct/src/main.rs
+++ learn_struct/src/main.rs
@@ -5,19 +5,34 @@ struct Rectangle {
 }
 
 impl Rectangle {
-    fn area(&self) -> u32 {
+    // reptiles call it `normal method` (in python)
+    fn area(&self) -> u32 { 
         self.width * self.height
     }
+
+    // reptiles call it `static method`(in python)
+    // crustacean call it `associative function(?)`
+    fn square(size: u32) -> Rectangle { 
+        Rectangle {width: size, height: size}
+    }
 }
+
+
 
 fn main() {
     let rect = Rectangle {
         width: 50,
         height: 30,
     };
+    let square = Rectangle::square(25);
     println!(
         "The rectangle is \n{:?},\n with area of {}",
         rect,
         rect.area()
+    );
+    println!(
+        "The square is \n{:?},\n with area of {}",
+        square,
+        square.area()
     )
 }
